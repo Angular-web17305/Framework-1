fb8e5d929b3ef1a4dcd096a93bcd4d6514f3caac
phanvanloi <106456389+phanvanloi@users.noreply.github.com>
2023-06-01 18:07:05 +0700

    update

--- assignment/src/app/app-routing.module.ts
+++ assignment/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomePageComponent, children: [
+      { path: 'product', component: ProductPageComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+    ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'product/add', component: ProductAddComponent },
+      { path: 'product/update/:id', component: ProductAddComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- assignment/src/app/app.module.ts
+++ assignment/src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
-import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductUpdateComponent } from './pages/product-update/product-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLayoutComponent,
     BaseLayoutComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomePageComponent,
+    ProductPageComponent,
+    ProductDetailComponent,
+    ProductAddComponent,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
