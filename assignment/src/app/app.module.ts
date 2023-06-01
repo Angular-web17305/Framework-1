bc558a190548ee4c873a9e2ae151a9b7a29bcdf4
phanvanloi <106456389+phanvanloi@users.noreply.github.com>
2023-06-01 16:42:05 +0700

    update

--- assignment/src/app/app.module.ts
+++ assignment/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
