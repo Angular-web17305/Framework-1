5f51cb31591cfbf1721686223c0c071a25a5ac81
HuynhPhamVan <128672412+HuynhPhamVan@users.noreply.github.com>
2023-06-08 23:14:02 +0700

    add category

--- assignment/src/app/pages/admin/add-category/add-category.component.ts
+++ assignment/src/app/pages/admin/add-category/add-category.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms'
-import { IProduct } from 'src/app/interface/product';
 import { ProductService } from 'src/app/service/product.service';
 import { Router } from '@angular/router';
 import { ICategory } from 'src/app/interface/categoryes';
