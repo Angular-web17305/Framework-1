ff65a033db2d71c6dccb1f25207b67f90179bacb
HuynhPhamVan <128672412+HuynhPhamVan@users.noreply.github.com>
2023-06-09 00:11:22 +0700

    list and delete

--- assignment/src/app/pages/admin/category-management/category-management.component.ts
+++ assignment/src/app/pages/admin/category-management/category-management.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ICategory } from 'src/app/interface/categoryes';
+import { CategoryesService } from 'src/app/service/categoryes.service';
 @Component({
   selector: 'app-category-management',
   templateUrl: './category-management.component.html',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class CategoryManagementComponent {
 
+   categorys!: ICategory[]
+  constructor(private CategoryesService: CategoryesService) {
+    CategoryesService.getCategorys().subscribe(data => this.categorys = data)
+  }
+
+  removeCate(id: number | string) {
+    const result = window.confirm("Bạn muốn xóa sản phẩm!")
+    if (result) {
+      this.CategoryesService.deleteCategory(id).subscribe(() => {
+        this.CategoryesService.getCategorys().subscribe(data => this.categorys = data)
+      })
+    }
+  }
 }
