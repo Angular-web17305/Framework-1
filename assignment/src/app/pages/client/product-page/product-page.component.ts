d2ab551f53018ebbc3abfe90763aa082c536a18e
phanvanloi <106456389+phanvanloi@users.noreply.github.com>
2023-06-09 01:03:12 +0700

    product-page

--- assignment/src/app/pages/client/product-page/product-page.component.ts
+++ assignment/src/app/pages/client/product-page/product-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ICategory } from 'src/app/interface/categoryes';
 import { IProduct } from 'src/app/interface/product';
+import { CategoryesService } from 'src/app/service/categoryes.service';
 import { ProductService } from 'src/app/service/product.service';
 @Component({
   selector: 'app-product-page',
@@ -10,12 +12,17 @@ import { ProductService } from 'src/app/service/product.service';
 
 export class ProductPageComponent {
   products!: IProduct[]
+  categoryes!: ICategory[]
   constructor(
     private router: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private categoryesService: CategoryesService
   ) {
     this.productService.getProducts().subscribe(data => {
       this.products = data
+    })
+    this.categoryesService.getCategorys().subscribe(data => {
+      this.categoryes = data
     })
   }
 }
