38072dc972ecebbbd7345098931302e44fac43e4
HuynhPhamVan <128672412+HuynhPhamVan@users.noreply.github.com>
2023-06-08 23:09:15 +0700

    add category

--- assignment/src/app/pages/admin/add-category/add-category.component.ts
+++ assignment/src/app/pages/admin/add-category/add-category.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
+import { IProduct } from 'src/app/interface/product';
+import { ProductService } from 'src/app/service/product.service';
+import { Router } from '@angular/router';
+import { ICategory } from 'src/app/interface/categoryes';
+import { CategoryesService } from 'src/app/service/categoryes.service';
+
 
 @Component({
   selector: 'app-add-category',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-category.component.css']
 })
 export class AddCategoryComponent {
+  categoryes!: ICategory[]
 
+  productForm = this.formBuilder.group(
+    {
+      name: [''],
+    }
+  )
+
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryesService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.categoryService.getCategorys().subscribe(data => this.categoryes = data)
+  }
+
+
+  onHandleSubmit() {
+    const category: ICategory = {
+      name: this.productForm.value.name || "",
+
+    }
+
+    this.categoryService.addCategory(category).subscribe(() => {
+      alert("Thêm category thành công")
+      this.router.navigateByUrl('/admin/category-management')
+    })
+  }
 }
+
