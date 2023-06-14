95a80725010b9cf8e0e2ca3465176a18aaffa866
LeCuc0609 <cucltph24757@fpt.edu.vn>
2023-06-14 14:43:12 +0700

    update-oder

--- assignment/src/app/pages/client/buy-item/buy-item.component.ts
+++ assignment/src/app/pages/client/buy-item/buy-item.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from 'src/app/interface/product';
+import { ICategory } from 'src/app/interface/categoryes';
+import { CategoryesService } from 'src/app/service/categoryes.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-buy-item',
@@ -6,5 +11,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./buy-item.component.css']
 })
 export class BuyItemComponent {
-
+  product: IProduct[] = [];
+  constructor(private productService: ProductService) {
+    this.productService.getProducts().subscribe(data => {
+      this.product = data;
+    });
+  }
 }
